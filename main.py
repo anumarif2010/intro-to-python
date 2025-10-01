abb411dc026f052baac3210b9238007a5705e2ac
anumarif2010 <muhammadarifganatra@gmail.com>
2025-10-01 21:42:10 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#this is my print statment
+print('heloo world')
+print(2010)
+print("heloo","its anum")
+print("how are you",16)
+print("i am \nfine")
+print("i am good",end=".")
+x=17
+print("\n",x)
+import keyword
+print(keyword.kwlist)
